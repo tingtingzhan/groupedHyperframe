04b67bf55f3b258431ec74b745769c73d1cacbd4
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-31 13:09:40 -0400

    tzh

--- R/data_doc.R
+++ R/data_doc.R
@@ -5,13 +5,21 @@
 #' @description 
 #' Ki67 cell data containing 622 patients.
 #' 
+#' @details
+#' The [groupedHyperframe] `Ki67` is the complete data, to be used in examples of downstream packages
+#' \pkg{maxEff} and \pkg{hyper.gam}.
+#' 
+#' The \link[base]{data.frame} `Ki67.` is a \link[base]{subset} of the complete data `Ki67`,
+#' which consists only the first 6 patients.  This small data is used as examples for function [as.hyperframe.data.frame()] 
+#' 
 #' @format
+#' 
 #' \describe{
 #'   \item{`patientID`}{\link[base]{factor}, unique patient identifier}
 #'   \item{`tissueID`}{\link[base]{factor}, TMA core identifier}
 #'   \item{`recurrence`}{\link[base]{integer}, recurrence indicator, 1 = Recurred, 0 = not Recurred}
 #'   \item{`recfreesurv_mon`}{\link[base]{integer}, recurrence-free survival time in months}
-#'   \item{`Ki67`}{\link[base]{double}, cell signal intensity of the protein immunofloerscence signal}
+#'   \item{`logKi67`}{\link[base]{double}, log-transformed cell signal intensity of the protein immunofloerscence signal}
 #   \item{`x`}{\link[base]{numeric}, \eqn{x}-coordinate in the cell centroid in the TMA core}
 #   \item{`y`}{\link[base]{numeric}, \eqn{y}-coordinate in the cell centroid in the TMA core}
 #'   \item{`age`}{\link[base]{integer}, age at diagnosis}
