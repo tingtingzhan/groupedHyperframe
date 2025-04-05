675e7c48f7feee4abbdd76230861f3bfd0f2845b
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-05 16:40:33 -0400

    tzh

--- R/cor_spatial.R
+++ R/cor_spatial.R
@@ -8,6 +8,9 @@
 #' 
 #' @param ... additional parameters, currently not in use
 #' 
+#' @details
+#' Workhorse function \link[SpatialPack]{cor.spatial}.
+#' 
 #' @returns 
 #' Function [pairwise_cor_spatial()] returns a \link[stats]{dist} object.
 #' 
@@ -19,8 +22,8 @@ pairwise_cor_spatial <- function(x, ...) UseMethod(generic = 'cor_spatial')
 #' @rdname pairwise_cor_spatial
 #' @examples
 #' data(finpines, package = 'spatstat.data')
-#' finpines |> pairwise_cor_spatial.ppp(formula = ~ diameter + height)
-#'
+#' (r = finpines |> pairwise_cor_spatial.ppp(formula = ~ diameter + height))
+#' r |> as.matrix()
 #' @importFrom SpatialPack cor.spatial
 #' @export pairwise_cor_spatial.ppp
 #' @export
@@ -51,5 +54,13 @@ pairwise_cor_spatial.ppp <- function(x, formula, ...) {
   class(ret) <- c('pairwise_cor_spatial', 'dist') # ?stats::dist
   return(ret)
   
+}
+
+
+#' @export
+as.matrix.pairwise_cor_spatial <- function(x, ...) {
+  ret <- NextMethod(generic = 'as.matrix') # invokes ?stats:::as.matrix.dist
+  diag(ret) <- 1 # ?SpatialPack::cor.spatial returns `1` for `identical(x, y)`
+  return(ret)
 }
 
