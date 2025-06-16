f21c01c38e2ad9eaf17973c55b33dca61e64297f
Tingting Zhan <tingtingzhan@gmail.com>
2025-06-15 23:22:35 -0400

    tzh

--- R/onLoad_parallel.R
+++ R/onLoad_parallel.R
@@ -3,8 +3,8 @@
 .onLoad <- function(libname, pkgname) {
   
   Sys.setenv(
-    # '_R_CHECK_LIMIT_CORES_' = 'false',
-    # otherwise ?parallel:::.check_ncores causes error when ?devtools::check
+    '_R_CHECK_LIMIT_CORES_' = 'false',
+    # otherwise ?parallel:::.check_ncores causes error when ?devtools::check the quarto vignette (up to R 4.5.1)
     
     '_R_CHECK_SYSTEM_CLOCK_' = 0
     # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time
