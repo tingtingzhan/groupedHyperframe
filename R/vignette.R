5b28636f63120255129483c47aca3e4364549133
Tingting Zhan <tingtingzhan@gmail.com>
2025-09-22 15:41:19 -0400

    tzh

---
+++ R/vignette.R
@@ -0,0 +1,28 @@
+
+
+#' @title `S3` methods table in vignette
+#' 
+#' @param class \link[base]{character} scalar
+#' 
+#' @param package \link[base]{character} scalar
+#' 
+#' @param ... additional parameters, currently not in use
+#' 
+#' @returns 
+#' Function [vignette_methods()] returns a \link[base]{data.frame}.
+#' 
+#' @keywords internal
+#' @importFrom utils methods
+#' @export
+vignette_methods <- function(class, package, ...) {
+  x <- methods(class = class) |> 
+    attr(which = 'info', exact = TRUE) |>
+    subset.data.frame(subset = eval(quote(from == package))) |>
+    within.data.frame(expr = {
+      generic = generic |> sprintf(fmt = '`%s`')
+    })
+  rownames(x) <- x |>
+    rownames() |> 
+    sprintf(fmt = '`%s`')
+  return(x)
+}
