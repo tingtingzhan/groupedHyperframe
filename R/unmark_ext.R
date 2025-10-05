fdb092da759b4349fc56e8d49158f41806179935
Tingting Zhan <tingtingzhan@gmail.com>
2025-10-05 10:01:27 -0400

    tzh

--- R/unmark_ext.R
+++ R/unmark_ext.R
@@ -1,19 +1,24 @@
 
-
-
-#' @title [unmark.ppplist()]
+#' @title Batch Process of \link[spatstat.geom]{unmark}
 #' 
-#' @param X a `'ppplist'`
-#' 
-#' @details
-#' Function [unmark.ppplist()] is a iteration of \link[spatstat.geom]{unmark.ppp},
-#' but not as yet \link[spatstat.geom]{unmark.splitppp}.
+#' @param X see **Usage**
 #' 
 #' @keywords internal
-#' @importFrom spatstat.geom unmark unmark.ppp solapply
-#' @export unmark.ppplist
+#' @name unmark_
 #' @export
-unmark.ppplist <- function(X) {
+unmark_ <- function(X) UseMethod(generic = 'unmark_')
+
+#' @rdname unmark_
+#' 
+#' @details
+#' Function [unmark_.ppplist()] is a iteration of \link[spatstat.geom]{unmark.ppp}.
+#' 
+#' Note that Dr. Baddeley has told me that \link[spatstat.geom]{unmark.splitppp} is a bandage fix.
+#' 
+#' @importFrom spatstat.geom unmark unmark.ppp solapply
+#' @export unmark_.ppplist
+#' @export
+unmark_.ppplist <- function(X) {
   X |>
     solapply(FUN = unmark.ppp)
 }
