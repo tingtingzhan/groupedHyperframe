07b5266809dfe602320c15c91340eeddba868936
Tingting Zhan <tingtingzhan@gmail.com>
2025-10-19 11:08:14 -0400

    tzh

--- R/Defunct.R
+++ R/Defunct.R
@@ -10,30 +10,49 @@
 #' Internal helper functions, 
 #' to display beautiful \link[base]{.Defunct} messages using package \CRANpkg{cli}.
 #' 
-#' @param x \link[base]{character} scalar
+#' @param author \link[base]{character} scalar
+#' 
+#' @param pub \link[base]{character} scalar
+#' 
+#' @param chapter \link[base]{character} scalar
+#' 
+#' @param doi \link[base]{character} scalar
 #' 
 #' @examples
-#' cli_RPubs_('tingtingzhan/groupedHyperframe')
+#' cli_RPubs_(pub = 'groupedHyperframe')
+#' cli_QuartoPub_(pub = 'groupedhyperframe')
+#' cli_QuartoPub_(pub = 'groupedhyperframe', chapter = 'bioinformatics_btaf430')
 #' cli_doi_('10.1002/bimj.4710230408')
 #' 
 #' @keywords internal
 #' @name cli_
 #' @export
-cli_RPubs_ <- function(x) {
-  x |>
-    sprintf(fmt = '{.url https://rpubs.com/%s}') |>
+cli_RPubs_ <- function(author = 'tingtingzhan', pub) {
+  sprintf(fmt = '{.url https://rpubs.com/%s/%s}', author, pub) |>
     cli_text()
 }
 
 #' @rdname cli_
 #' @export
-cli_doi_ <- function(x) {
+cli_doi_ <- function(doi) {
   # `x`: 'character' scalar of doi
-  sprintf(fmt = '{.href [doi:%s](https://doi.org/%s)}', x, x) |>
+  sprintf(fmt = '{.href [doi:%s](https://doi.org/%s)}', doi, doi) |>
     cli_text()
 }
 
 
+#' @rdname cli_
+#' @export
+cli_QuartoPub_ <- function(author = 'tingtingzhan', pub, chapter) {
+  if (missing(chapter)) {
+    sprintf(fmt = '{.url https://%s.quarto.pub/%s}', author, pub) |>
+      cli_text()
+  } else {
+    sprintf(fmt = '{.url https://%s.quarto.pub/%s/%s.html}', author, pub, chapter) |>
+      cli_text()
+  }
+  
+}
 
 #' @title Defunct Functions
 #' 
@@ -57,7 +76,7 @@ aggregate_quantile <- function(new = '<groupedHyperframe> |> quantile() |> aggre
     message()
   
   'Read vignette for details' |> message()
-  cli_RPubs_('tingtingzhan/groupedHyperframe')
+  cli_RPubs_(pub = 'groupedHyperframe')
 
   .Defunct(new = new)
   
@@ -76,7 +95,7 @@ aggregate_fv <- function(new = '<groupedHyperframe> |> summary_fv() |> aggregate
     message()
   
   'Read vignette for details' |> message()
-  cli_RPubs_('tingtingzhan/groupedHyperframe')
+  cli_RPubs_(pub = 'groupedHyperframe')
   
   .Defunct(new = new)
 
