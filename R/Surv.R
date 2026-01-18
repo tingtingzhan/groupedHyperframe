fb1a955993f821c7f0237f29edd2b34c49293fa2
Tingting Zhan <tingtingzhan@gmail.com>
2026-01-18 12:03:44 -0500

    tzh

--- R/Surv.R
+++ R/Surv.R
@@ -16,19 +16,36 @@
 #'   os = survival::Surv(time = time, event = status)
 #'   time = status = NULL
 #'  })
+#' summary(aml2$os)
 #' summary(aml2)
+#' 
+#' heart2 = survival::heart |>
+#'  within.data.frame(expr = {
+#'   os = survival::Surv(time = start, time2 = stop, event = event)
+#'   start = stop = event = NULL
+#'  })
+#' summary(heart2$os)
+#' summary(heart2)
 #' 
 #' @keywords internal
 #' @export summary.Surv
 #' @export
 summary.Surv <- function(object, ...) {
-  z <- c('<time-to-event>' = '(Surv)') # read ?base::summary.data.frame very carefully!!
-  class(z) <- 'summary.Surv'
-  return(z)
+  
+  # read ?base::summary.data.frame very carefully!!
+  
+  z <- if (ncol(object) == 2L) {
+    (object[,2L] + 1L) |>
+      structure(levels = c('[right-censored]', '[observed]'), class = 'factor') |>
+      summary.factor()
+  } else if (ncol(object) == 3L) {
+    return(NextMethod(generic = 'summary')) # tzh doesnt know left-censoring and interval-censoring too well..
+  } else stop('shouldnt happen')
+  
+  return(c(
+    '<time-to-event>' = '(Surv)',
+    z
+  ))
+  
 }
 
-#' @export
-print.summary.Surv <- function(x, ...) {
-  x[] |> # drops extra class
-    print.default()
-}
