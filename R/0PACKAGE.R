e08646ee5b0604ade98f559ad5c40417a3cf8d42
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-09 00:27:03 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -23,5 +23,31 @@ if (FALSE) {
 # ?utils::citation does *not* need to Imports the package!!
 
 
+.onAttach <- function(libname, pkgname) {
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  sprintf(
+    fmt = 'Welcome to R package {.href [groupedHyperframe](https://cran.r-project.org/package=groupedHyperframe)}, an %s project by {.href [%s](https://github.com/tingtingzhan/groupedHyperframe)}',
+    'entertaining-but-useless' |> bg_br_yellow(),
+    'Tingting Zhan' |> col_red() |> style_bold()
+  ) |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  sprintf(
+    fmt = 'Please read the {.href [%s](https://tingtingzhan.quarto.pub/groupedhyperframe/)} for details.',
+    'vignettes' |> col_blue() |> style_bold()
+  ) |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+}
+
 
 
