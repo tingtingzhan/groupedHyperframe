dfb6c382ce1aecee0fecfafa31cb4eb472fde35e
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-15 11:25:16 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -22,16 +22,31 @@ if (FALSE) {
 # requires installed package from CRAN..
 # ?utils::citation does *not* need to Imports the package!!
 
-
+#' @importFrom utils packageVersion packageDate packageDescription
 .onAttach <- function(libname, pkgname) {
   
   '\n' |>
     cli_inform(class = 'packageStartupMessage')
   
+  'Welcome to Package {.href [groupedHyperframe](https://cran.r-project.org/package=groupedHyperframe)}' |>
+    cli_inform(class = 'packageStartupMessage')
+  
   sprintf(
-    fmt = 'Welcome to R package {.href [groupedHyperframe](https://cran.r-project.org/package=groupedHyperframe)}, an %s project by {.href [%s](https://github.com/tingtingzhan/groupedHyperframe)}',
-    'entertaining-but-useless' |> bg_br_yellow(),
-    'Tingting Zhan' |> col_red() |> style_bold()
+    fmt = 'version %s (%s) -- %s',
+    'groupedHyperframe' |> packageVersion(),
+    'groupedHyperframe' |> packageDate(),
+    packageDescription('groupedHyperframe')[['Nickname']] |> 
+      dQuote() |>
+      style_bold() |>
+      bg_br_yellow()
+  ) |>
+    cli_inform(class = 'packageStartupMessage')
+
+  sprintf(
+    fmt = 'by {.href [%s](https://github.com/tingtingzhan/groupedHyperframe)}',
+    'Tingting Zhan' |> 
+      col_red() |> 
+      style_bold()
   ) |>
     cli_inform(class = 'packageStartupMessage')
   
