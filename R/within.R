33e76c02486281d4a79b975c5a7d05386dd62bce
Tingting Zhan <tingtingzhan@Tingtings-MacBook-Air.local>
2026-03-21 01:25:43 -0400

    tzh

--- R/within.R
+++ R/within.R
@@ -18,15 +18,25 @@ within.hyperframe <- function(
     if (!(as.character(cl[[1L]]) %in% c('=', '<-'))) stop()
     if (!is.symbol(cl[[2L]])) stop()
     
+    tmp <- 
+      # with.hyperframe(
+      with_hyperframe_mc(
+        data, ee = cl[[3L]], simplify = FALSE
+      )
+    tmp_n <- tmp |>
+      lengths(use.names = FALSE)
+    tmp_vec <- tmp |>
+      vapply(FUN = is.vector, FUN.VALUE = NA)
+    if (all(tmp_vec) && all(tmp_n == 1L)) {
+      tmp <- tmp |>
+        unlist(use.names = FALSE)
+    }
+    
     # to create a **new** (hyper)column,
     # spatstat.geom::`[[<-.hyperframe` # does not work!!
     # spatstat.geom::`$<-.hyperframe` # must use!!!
     call(name = '$', quote(data), cl[[2L]]) |>
-      call(
-        name = '<-', . = _, 
-        #call(name = 'with.hyperframe', data = quote(data), expr = cl[[3L]], simplify = FALSE)
-        call(name = 'with_hyperframe_mc', data = quote(data), expr = cl[[3L]], simplify = FALSE)
-        ) |>
+      call(name = '<-', . = _, quote(tmp)) |>
       eval()
     
   }
