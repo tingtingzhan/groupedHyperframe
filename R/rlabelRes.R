0a1f00844881b799cbb8a0d1ba1293c20fd0bb71
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-21 12:56:05 -0400

    tzh

--- R/rlabelRes.R
+++ R/rlabelRes.R
@@ -41,21 +41,6 @@
 rlabelRes <- function(x, ...) UseMethod(generic = 'rlabelRes')
 
 
-#' @title Global Envelope Test in Bath Process
-#' 
-#' @param x see **Usage**
-#' 
-#' @param ... parameters (except the first) of the function \link[GET]{global_envelope_test}
-#' 
-#' @name global_envelope_test_
-#' @keywords internal
-#' @export
-global_envelope_test_ <- function(x, ...) UseMethod(generic = 'global_envelope_test_')
-
-
-
-
-
 #' @rdname rlabelRes
 #' @importFrom spatstat.explore envelope.ppp
 #' @importFrom spatstat.random rlabel
@@ -72,73 +57,5 @@ rlabelRes.ppp <- function(x, ...) {
     residual() # c('curve_set', 'list')
 }
 
-# 
-# |> # 
-#global_envelope_test() # c('global_envelope', 'data.frame')
-
-#' @rdname rlabelRes
-#' @importFrom spatstat.geom anylapply
-#' @export
-rlabelRes.ppplist <- function(x, ...) {
-  x |>
-    anylapply(FUN = rlabelRes.ppp, ...)
-}
 
 
-
-#' @rdname global_envelope_test_
-#' @importFrom GET global_envelope_test
-#' @export
-global_envelope_test_.anylist <- function(x, ...) {
-  id <- x |>
-    vapply(FUN = inherits, what = 'curve_set', FUN.VALUE = NA)
-  if (!all(id)) return(invisible()) # exception handling
-  x |>
-    anylapply(FUN = global_envelope_test, ...)
-}
-
-
-#' @rdname rlabelRes
-#' @export
-rlabelRes.hyperframe <- function(x, ...) {
-  
-  hc <- unclass(x)$hypercolumns
-  
-  hc_ppp <- hc |>
-    vapply(FUN = is.ppplist, FUN.VALUE = NA)
-  n_ppp <- sum(hc_ppp)
-  if (!n_ppp) return(invisible()) # exception handling
-  if (n_ppp > 1L) stop('does not allow more than 1 ppp-hypercolumn')
-  
-  z <- tryCatch(expr = {
-    hc[[which(hc_ppp)]] |>
-      rlabelRes.ppplist(...)
-  }, error = identity)
-  if (inherits(z, what = 'error')) return(x) # exception handling
-  
-  return(cbind( # dispatch to \link[spatstat.geom]{cbind.hyperframe} or [cbind.groupedHyperframe()]
-    x, 
-    .rlabelRes = z
-  ))
-  
-}
-
-#' @rdname global_envelope_test_
-#' @importFrom spatstat.geom names.hyperframe names<-.hyperframe
-#' @export
-global_envelope_test_.hyperframe <- function(x, ...) {
-  
-  hc <- unclass(x)$hypercolumns
-  
-  z0 <- hc |>
-    lapply(FUN = global_envelope_test_.anylist, ...)
-  z <- z0[lengths(z0, use.names = FALSE) > 0L] |>
-    do.call(what = hyperframe, args = _)
-  names(z) <- paste(names.hyperframe(z), 'GET', sep = '.')
-  
-  return(cbind( # dispatch to \link[spatstat.geom]{cbind.hyperframe} or [cbind.groupedHyperframe()]
-    x, 
-    z
-  ))
-  
-}
