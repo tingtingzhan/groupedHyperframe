663f3d41d955ebfdcece2f5e5301957c38755607
Tingting Zhan <tingtingzhan@gmail.com>
2026-05-04 16:59:31 -0400

    tzh

--- R/aug4gam.R
+++ R/aug4gam.R
@@ -1,85 +1,43 @@
 
-# ?hyper.gam::augdata will be replaced by aug4gam
-
-
 #' @title Augment Hypercolumn(s) for \link[mgcv]{gam}
 #' 
-#' @param data \link[spatstat.geom]{hyperframe}
+#' @description
+#' Augment *all* \link[base]{numeric} \link[base]{vector} hypercolumns in a \link[spatstat.geom]{hyperframe} 
+#' for \link[mgcv]{gam}.
 #' 
-# @param formula one-sided \link[stats]{formula} - this parameter will be removed!!
+#' 
+#' 
+#' @param x see **Usage**
 #' 
 #' @param ... additional parameters, currently of no use
-#' 
-#' @details
-#' All \link[base]{numeric} \link[base]{vector} hypercolumns will be augmented.
-#' 
 #' 
 #' @author 
 #' Tingting Zhan, Erjia Cui
 #' 
 #' @name aug4gam
 #' @export
-aug4gam <- function(data, ...) UseMethod(generic = 'aug4gam')
+aug4gam <- function(x, ...) UseMethod(generic = 'aug4gam')
 
 #' @rdname aug4gam
 #' @export
-aug4gam.data.frame <- function(data, ...) return(data)
-
-
-.aug4gam_numeric_vectorlist <- \(x, nm) {
-  X <- x |> 
-    do.call(what = rbind)
-  
-  cnm <- X |> 
-    colnames()
-  x. <- if (all(grepl(pattern = '%$', x = cnm))) {
-    # returned from ?stats:::quantile.default
-    tmp <- cnm |>
-      gsub(pattern = '%$', replacement = '') |>
-      as.double()
-    tmp / 1e2
-  } else cnm |> as.double() 
-  
-  if (!length(x.) || !is.numeric(x.) || anyNA(x.) || is.unsorted(x., strictly = TRUE)) {
-    stop(nm |> col_blue(), ' must have names convertible to strictly-increasing numerics')
-  }
-  
-  colnames(X) <- x.
-  
-  X.x <- tcrossprod(rep(1, times = length(x)), x.)
-  nc <- length(x.)
-  X.L <- array(1/nc, dim = dim(X))
-  
-  list(X, X.x, X.L) |>
-    setNames(nm = c(
-      nm, 
-      paste(nm, 'x', sep = '.'),
-      paste(nm, 'L', sep = '.')
-    ))
-  
-}
-
+aug4gam.data.frame <- function(x, ...) return(x)
 
 
 #' @rdname aug4gam
 #' @export
-aug4gam.hyperframe <- function(data, ...) {
+aug4gam.hyperframe <- function(x, ...) {
   
-  hc <- unclass(data)$hypercolumns
+  hc <- unclass(x)$hypercolumns
   
   id <- hc |> 
     vapply(FUN = is.vectorlist, mode = 'numeric', FUN.VALUE = NA)
   if (!any(id)) stop('no vector-list hypercolumn')
 
-  z <- mapply(
-    FUN = .aug4gam_numeric_vectorlist, 
-    x = hc[id], nm = names(hc)[id], 
-    SIMPLIFY = FALSE
-  ) |>
-    unname() |>
+  z <- hc[id] |> 
+    lapply(FUN = aug4gam.vectorlist) |>
     unlist(recursive = FALSE)
   
-  dat <- unclass(data)$df
+  dat <- unclass(x)$df
   # seems that only
   # base::`$<-.data.frame`
   # base::`[[<-.data.frame`
@@ -90,5 +48,36 @@ aug4gam.hyperframe <- function(data, ...) {
   }
   
   return(dat)
+  
+}
+
+#' @rdname aug4gam
+#' @export
+aug4gam.vectorlist <- function(x, ...) {
+  
+  Y <- x |> 
+    do.call(what = rbind)
+  
+  cnm <- Y |> 
+    colnames()
+  x. <- if (all(grepl(pattern = '%$', x = cnm))) {
+    # returned from ?stats:::quantile.default
+    tmp <- cnm |>
+      gsub(pattern = '%$', replacement = '') |>
+      as.double()
+    tmp / 1e2
+  } else cnm |> as.double() 
+  
+  if (!length(x.) || !is.numeric(x.) || anyNA(x.) || is.unsorted(x., strictly = TRUE)) {
+    stop('must have names convertible to strictly-increasing numerics')
+  }
+  
+  colnames(Y) <- x.
+  
+  X <- tcrossprod(rep(1, times = length(x)), x.)
+  nc <- length(x.)
+  L <- array(1/nc, dim = dim(Y))
+  
+  list(x = X, y = Y, L = L)
   
 }
